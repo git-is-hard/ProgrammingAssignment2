7df39748c64c6352645367d1131fde14a6046b1d
git-is-hard <thaitran1410@icloud.com>
2022-03-22 00:09:30 +0700

    lexical scooping coursera data science

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## makeCacheMatrix receive a matrix, then save it to getMatrix.
+## saveInv receive inverse matrix then save it to global memory as cacheInv
+## checkCache return the value in cacheInv to check if there already have inverse
+## result or not.
 makeCacheMatrix <- function(x = matrix()) {
               cacheInv <- NULL
               getMatrix <- function() x
@@ -13,7 +16,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## first it take the result of function above then check if there are saved result
+## or not
+##else, below math  take the matrix by lexical scoping and return the result
+## and use saveInv to save result in cacheInv
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   check <- x$checkCache()
